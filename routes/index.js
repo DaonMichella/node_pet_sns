7351cc97dacd6f04c9947a38c63ab228aa62d5c8
jeonsooyoung <peace2332@naver.com>
2021-04-29 07:42:37 +0900

    s

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var router = express.Router();
+const authRouter = require('./auth');
+const postRouter = require('./posts');
 
 router.get('/',(req,res) => {
     res.render('index',{postList : []})//index.js 보여줌 postList 를 통해서 게시물 정보 보여줌
 })
+
+router.use("/auth", authRouter);
+router.use("/posts", postRouter);
 
 module.exports = router;
